64411b401a0e6a1d3c7c449f723fe5e6e216421a
Koichi5 <koichi20021217@gmail.com>
2023-09-10 02:46:30 +0900

    edit device width and height

--- lib/view/auth/screens/signup_screen.dart
+++ lib/view/auth/screens/signup_screen.dart
@@ -1,5 +1,5 @@
 import 'package:emo_project/view/auth/components/apple_signin_button.dart';
-import 'package:emo_project/view/auth/components/custom_textfield.dart';
+import 'package:emo_project/view/common/components/custom_textfield.dart';
 import 'package:emo_project/view/auth/components/google_signin_button.dart';
 import 'package:emo_project/view/auth/screens/login_screen.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +10,8 @@ class SignupScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final double deviceHeight = MediaQuery.of(context).size.height;
+    final double deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -25,39 +27,39 @@ class SignupScreen extends ConsumerWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.05,
+                  height: deviceHeight * 0.05,
                 ),
                 SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.9,
+                  width: deviceWidth * 0.9,
                   child: const CustomTextField(
                     title: "メールアドレス",
                   ),
                 ),
                 SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.04,
+                  height: deviceHeight * 0.04,
                 ),
                 SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.9,
+                  width: deviceWidth * 0.9,
                   child: const CustomTextField(
                     title: "パスワード",
                   ),
                 ),
                 SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.04,
+                  height: deviceHeight * 0.04,
                 ),
                 SizedBox(
                   // height: 40,
-                  width: MediaQuery.of(context).size.width * 0.9,
+                  width: deviceWidth * 0.9,
                   child: ElevatedButton(
                     onPressed: () {},
                     child: const Text("新規登録"),
                   ),
                 ),
                 Divider(
-                  height: MediaQuery.of(context).size.height * 0.08,
+                  height: deviceHeight * 0.08,
                   thickness: 0.5,
-                  indent: MediaQuery.of(context).size.width * 0.05,
-                  endIndent: MediaQuery.of(context).size.width * 0.05,
+                  indent: deviceWidth * 0.05,
+                  endIndent: deviceWidth * 0.05,
                   color: Theme.of(context).colorScheme.primary,
                 ),
                 const Padding(
@@ -71,7 +73,7 @@ class SignupScreen extends ConsumerWidget {
                 Padding(
                   padding: const EdgeInsets.all(5.0),
                   child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.9,
+                    width: deviceWidth * 0.9,
                     child: TextButton(
                       onPressed: () {
                         Navigator.push(
