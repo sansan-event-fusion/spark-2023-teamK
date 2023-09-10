353c6011429df1a28f506bc8c19804a8cac7dc45
Koichi5 <koichi20021217@gmail.com>
2023-09-10 14:26:28 +0900

    edit group search function

--- lib/view/group/screens/join_group_screen.dart
+++ lib/view/group/screens/join_group_screen.dart
@@ -11,6 +11,8 @@ final List<String> groupImageList = [
   "https://cdn.pixabay.com/photo/2013/03/19/18/23/mountain-biking-95032_1280.jpg",
   "https://cdn.pixabay.com/photo/2017/04/26/17/58/road-bike-2263202_1280.jpg",
   "https://cdn.pixabay.com/photo/2018/06/11/21/50/road-bike-3469503_1280.jpg",
+  "https://cdn.pixabay.com/photo/2020/08/09/16/23/cyclists-5475979_1280.jpg",
+  "https://cdn.pixabay.com/photo/2016/11/10/14/00/cycling-1814362_1280.jpg",
 ];
 
 final List<String> groupNameList = [
@@ -22,6 +24,21 @@ final List<String> groupNameList = [
   "Group F",
   "Group G",
   "Group H",
+  "Group I",
+  "Group J",
+];
+
+final List<String> groupIdList = [
+  "12345A",
+  "12345B",
+  "12345C",
+  "12345D",
+  "12345E",
+  "12345F",
+  "12345G",
+  "12345H",
+  "12345I",
+  "12345J",
 ];
 
 final onSearchProvider = StateProvider((ref) => false);
@@ -43,33 +60,36 @@ class JoinGroupScreen extends ConsumerWidget {
       ),
       body: Column(
         children: [
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            onSearch
-                ? Expanded(child: _searchTextField(ref))
-                : Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: GestureDetector(
-                      onTap: () {
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              onSearch
+                  ? Expanded(child: _searchTextField(ref))
+                  : Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          onSearchNotifier.state = true;
+                          searchIndexListNotifier.state = [];
+                        },
+                        child: const Text("グループをIDで検索"),
+                      ),
+                    ),
+              onSearch
+                  ? IconButton(
+                      onPressed: () {
+                        onSearchNotifier.state = false;
+                      },
+                      icon: const Icon(Icons.clear))
+                  : IconButton(
+                      onPressed: () {
                         onSearchNotifier.state = true;
                         searchIndexListNotifier.state = [];
                       },
-                      child: const Text("グループ検索"),
+                      icon: const Icon(Icons.search),
                     ),
-                  ),
-            onSearch
-                ? IconButton(
-                    onPressed: () {
-                      onSearchNotifier.state = false;
-                    },
-                    icon: const Icon(Icons.clear))
-                : IconButton(
-                    onPressed: () {
-                      onSearchNotifier.state = true;
-                      searchIndexListNotifier.state = [];
-                    },
-                    icon: const Icon(Icons.search),
-                  ),
-          ]),
+            ],
+          ),
           onSearch ? _searchListView(ref) : _defaultListView(),
         ],
       ),
@@ -84,8 +104,8 @@ class JoinGroupScreen extends ConsumerWidget {
         autofocus: true,
         onChanged: (String text) {
           searchIndexListNotifier.state = [];
-          for (int i = 0; i < groupNameList.length; i++) {
-            if (groupNameList[i].contains(text)) {
+          for (int i = 0; i < groupIdList.length; i++) {
+            if (text == groupIdList[i]) {
               searchIndexListNotifier.state.add(i);
             }
           }
@@ -97,12 +117,45 @@ class JoinGroupScreen extends ConsumerWidget {
   Widget _searchListView(WidgetRef ref) {
     final searchIndexListNotifier = ref.watch(searchIndexListProvider.notifier);
     final searchIndexList = ref.watch(searchIndexListProvider);
-    return ListView.builder(
-        itemCount: searchIndexList.length,
+    return Flexible(
+      child: ListView.builder(
+          itemCount: searchIndexList.length,
+          shrinkWrap: true,
+          itemBuilder: (context, int index) {
+            index = searchIndexListNotifier.state[index];
+            return Card(
+              child: ListTile(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          GroupProfileScreen(groupName: groupNameList[index]),
+                    ),
+                  );
+                },
+                leading: Image.network(
+                  groupImageList[index],
+                  width: 50,
+                  height: 50,
+                ),
+                title: Text(groupNameList[index]),
+                subtitle: Text(
+                  "ID: ${groupIdList[index]}",
+                  style: const TextStyle(color: Colors.grey),
+                ),
+              ),
+            );
+          }),
+    );
+  }
+
+  Widget _defaultListView() {
+    return Flexible(
+      child: ListView.builder(
         shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        itemBuilder: (context, int index) {
-          index = searchIndexListNotifier.state[index];
+        itemCount: groupNameList.length,
+        itemBuilder: (context, index) {
           return Card(
             child: ListTile(
               onTap: () {
@@ -122,35 +175,8 @@ class JoinGroupScreen extends ConsumerWidget {
               title: Text(groupNameList[index]),
             ),
           );
-        });
-  }
-
-  Widget _defaultListView() {
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      itemCount: groupNameList.length,
-      itemBuilder: (context, index) {
-        return Card(
-          child: ListTile(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      GroupProfileScreen(groupName: groupNameList[index]),
-                ),
-              );
-            },
-            leading: Image.network(
-              groupImageList[index],
-              width: 50,
-              height: 50,
-            ),
-            title: Text(groupNameList[index]),
-          ),
-        );
-      },
+        },
+      ),
     );
   }
 }
