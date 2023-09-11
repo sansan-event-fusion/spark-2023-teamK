1dff9659167e0b736b31261148deb645a2a9d443
Koichi5 <koichi20021217@gmail.com>
2023-09-11 23:51:53 +0900

    add auth repository and controller

--- lib/view/auth/screens/signup_screen.dart
+++ lib/view/auth/screens/signup_screen.dart
@@ -1,3 +1,4 @@
+import 'package:emo_project/controller/auth/auth_controller.dart';
 import 'package:emo_project/view/auth/components/apple_signin_button.dart';
 import 'package:emo_project/view/common/components/custom_textfield.dart';
 import 'package:emo_project/view/auth/components/google_signin_button.dart';
@@ -6,6 +7,14 @@ import 'package:emo_project/view/initial/screens/initial_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final signupEmailControllerStateProvider = StateProvider.autoDispose((ref) {
+  return TextEditingController(text: '');
+});
+
+final signupPasswordControllerStateProvider = StateProvider.autoDispose((ref) {
+  return TextEditingController(text: '');
+});
+
 class SignupScreen extends ConsumerWidget {
   const SignupScreen({Key? key}) : super(key: key);
 
@@ -13,6 +22,8 @@ class SignupScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final double deviceHeight = MediaQuery.of(context).size.height;
     final double deviceWidth = MediaQuery.of(context).size.width;
+    final emailController = ref.watch(signupEmailControllerStateProvider);
+    final passwordController = ref.watch(signupPasswordControllerStateProvider);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -32,8 +43,9 @@ class SignupScreen extends ConsumerWidget {
                 ),
                 SizedBox(
                   width: deviceWidth * 0.9,
-                  child: const CustomTextField(
+                  child: CustomTextField(
                     title: "メールアドレス",
+                    controller: emailController,
                   ),
                 ),
                 SizedBox(
@@ -41,18 +53,22 @@ class SignupScreen extends ConsumerWidget {
                 ),
                 SizedBox(
                   width: deviceWidth * 0.9,
-                  child: const CustomTextField(
+                  child: CustomTextField(
                     title: "パスワード",
+                    controller: passwordController,
                   ),
                 ),
                 SizedBox(
                   height: deviceHeight * 0.04,
                 ),
                 SizedBox(
-                  // height: 40,
                   width: deviceWidth * 0.9,
                   child: ElevatedButton(
                     onPressed: () {
+                      ref
+                          .watch(authControllerProvider.notifier)
+                          .createUserWithEmailAndPassword(
+                              emailController.text, passwordController.text);
                       Navigator.push(
                         context,
                         MaterialPageRoute(
