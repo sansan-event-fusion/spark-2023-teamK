2254edeb822b7cfcb5141b886f4b24b6a832008f
yona3 <yonajs208@gmail.com>
2023-09-12 15:20:12 +0900

    wip: add deleteMember

--- functions/src/lib/firebase.ts
+++ functions/src/lib/firebase.ts
@@ -2,6 +2,6 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
 admin.initializeApp();
-const firestore = admin.firestore();
+const firestore = admin.firestore;
 
 export { functions, firestore };
--- functions/src/modules/createGroup.ts
+++ functions/src/modules/createGroup.ts
@@ -1,18 +1,31 @@
 import { HttpHandler } from "../types";
+import * as admin from "firebase-admin";
 
 type RequestData = {
   userId: string;
 };
 
 type ResponseData = {
-  message: string;
+  success: boolean;
+  id: string;
 };
 
-export const createGroup: HttpHandler<RequestData, ResponseData> = (
+export const createGroup: HttpHandler<RequestData, ResponseData> = async (
   data,
   _
 ) => {
-  return {
-    message: data.userId,
+  const { userId } = data;
+
+  const postData = {
+    userId,
   };
+
+  try {
+    const docRef = await admin.firestore().collection("groups").add(postData);
+
+    return { success: true, id: docRef.id };
+  } catch (error) {
+    return { success: false, id: "aa" };
+    //error追加したい
+  }
 };
