4e105789082bed4e49f8ec9b244617f189f19fd4
Koichi5 <koichi20021217@gmail.com>
2023-09-12 14:37:18 +0900

    add riverpod annotation

--- lib/controller/member/member_controller.dart
+++ lib/controller/member/member_controller.dart
@@ -1,25 +1,23 @@
 import 'package:emo_project/model/member/member.dart';
 import 'package:emo_project/model/repository/member_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-final memberControllerProvider = StateNotifierProvider((ref) {
-  return MemberController(ref);
-});
+part 'member_controller.g.dart';
 
-class MemberController extends StateNotifier {
-  final Ref _ref;
-  MemberController(this._ref) : super(null);
+@riverpod
+class MemberController extends _$MemberController {
+  @override
+  Future<List<Member>> build({required String groupId}) async {
+    final members = await retrieveMembers();
+    return members;
+  }
 
-  Future<List<Member>> retrieveMembers(
-      {required String groupId}) async {
+  Future<List<Member>> retrieveMembers() async {
     try {
-      final members = await _ref
-          .watch(memberRepository)
-          .retrieveMembers(groupId: groupId);
-      if (mounted) {
-        state = AsyncValue.data(members);
-      }
+      final members =
+          await ref.watch(memberRepository).retrieveMembers(groupId: groupId);
+      state = AsyncValue.data(members);
       return members;
     } on FirebaseException catch (e) {
       throw Exception(e.message);
@@ -27,14 +25,11 @@ class MemberController extends StateNotifier {
   }
 
   Future<String> createMember(
-      {required Member member, required String groupId}) async {
+      {required Member member}) async {
     try {
-      final docRef = await _ref
+      final docRef = await ref
           .watch(memberRepository)
           .createMember(member: member, groupId: groupId);
-      if (mounted) {
-        state = AsyncValue.data(docRef);
-      }
       return docRef;
     } on FirebaseException catch (e) {
       throw Exception(e.message);
@@ -42,9 +37,9 @@ class MemberController extends StateNotifier {
   }
 
   Future<void> updateMember(
-      {required Member member, required String groupId}) async {
+      {required Member member}) async {
     try {
-      await _ref
+      await ref
           .watch(memberRepository)
           .updateMember(member: member, groupId: groupId);
     } on FirebaseException catch (e) {
@@ -53,9 +48,9 @@ class MemberController extends StateNotifier {
   }
 
   Future<void> deleteMember(
-      {required String memberId, required String groupId}) async {
+      {required String memberId}) async {
     try {
-      await _ref
+      await ref
           .watch(memberRepository)
           .deleteMember(memberId: memberId, groupId: groupId);
     } on FirebaseException catch (e) {
--- lib/view/auth/screens/signup_screen.dart
+++ lib/view/auth/screens/signup_screen.dart
@@ -1,7 +1,5 @@
-import 'package:emo_project/model/album/album.dart';
-import 'package:emo_project/model/album_picture/album_picture.dart';
-import 'package:emo_project/model/repository/album_picture_repository.dart';
-import 'package:emo_project/model/repository/album_repository.dart';
+import 'package:emo_project/controller/member/member_controller.dart';
+import 'package:emo_project/model/member/member.dart';
 import 'package:emo_project/view/auth/components/apple_signin_button.dart';
 import 'package:emo_project/view/common/components/custom_textfield.dart';
 import 'package:emo_project/view/auth/components/google_signin_button.dart';
@@ -17,118 +15,129 @@ class SignupScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final double deviceHeight = MediaQuery.of(context).size.height;
     final double deviceWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text("新規登録"),
-        automaticallyImplyLeading: false,
-      ),
-      body: Center(
-        child: Container(
-          alignment: Alignment.topCenter,
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: deviceHeight * 0.05,
-                ),
-                SizedBox(
-                  width: deviceWidth * 0.9,
-                  child: const CustomTextField(
-                    title: "メールアドレス",
-                  ),
-                ),
-                SizedBox(
-                  height: deviceHeight * 0.04,
-                ),
-                SizedBox(
-                  width: deviceWidth * 0.9,
-                  child: const CustomTextField(
-                    title: "パスワード",
-                  ),
-                ),
-                SizedBox(
-                  height: deviceHeight * 0.04,
-                ),
-                SizedBox(
-                  // height: 40,
-                  width: deviceWidth * 0.9,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const InitialScreen(),
+    final state = ref.watch(memberControllerProvider(groupId: "test"));
+    return state.when(
+        loading: () {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+        error: (error, stackTrace) {
+          return Text(error.toString());
+        },
+        data: (data) => Scaffold(
+              appBar: AppBar(
+                centerTitle: true,
+                title: const Text("新規登録"),
+                automaticallyImplyLeading: false,
+              ),
+              body: Center(
+                child: Container(
+                  alignment: Alignment.topCenter,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: deviceHeight * 0.05,
                         ),
-                      );
-                    },
-                    child: const Text("新規登録"),
-                  ),
-                ),
-                Divider(
-                  height: deviceHeight * 0.08,
-                  thickness: 0.5,
-                  indent: deviceWidth * 0.05,
-                  endIndent: deviceWidth * 0.05,
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(6.0),
-                  child: GoogleSignInButton(),
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(6.0),
-                  child: AppleSignInButton(),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: SizedBox(
-                    width: deviceWidth * 0.9,
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const LoginScreen(),
+                        SizedBox(
+                          width: deviceWidth * 0.9,
+                          child: const CustomTextField(
+                            title: "メールアドレス",
                           ),
-                        );
-                      },
-                      child: const Text(
-                        "アカウントをお持ちの方はこちら",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
+                        ),
+                        SizedBox(
+                          height: deviceHeight * 0.04,
+                        ),
+                        SizedBox(
+                          width: deviceWidth * 0.9,
+                          child: const CustomTextField(
+                            title: "パスワード",
+                          ),
+                        ),
+                        SizedBox(
+                          height: deviceHeight * 0.04,
+                        ),
+                        SizedBox(
+                          // height: 40,
+                          width: deviceWidth * 0.9,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const InitialScreen(),
+                                ),
+                              );
+                            },
+                            child: const Text("新規登録"),
+                          ),
+                        ),
+                        Divider(
+                          height: deviceHeight * 0.08,
+                          thickness: 0.5,
+                          indent: deviceWidth * 0.05,
+                          endIndent: deviceWidth * 0.05,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(6.0),
+                          child: GoogleSignInButton(),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(6.0),
+                          child: AppleSignInButton(),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: SizedBox(
+                            width: deviceWidth * 0.9,
+                            child: TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const LoginScreen(),
+                                  ),
+                                );
+                              },
+                              child: const Text(
+                                "アカウントをお持ちの方はこちら",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            print(data);
+                          },
+                          child: Text("member list test"),
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+                              ref
+                                  .read(
+                                      memberControllerProvider(groupId: "test")
+                                          .notifier)
+                                  .createMember(
+                                      member: Member(
+                                          memberId: "memberId2",
+                                          name: "name2",
+                                          role: "role2",
+                                          icon: "icon2",
+                                          description: "description2",
+                                          createdAt: DateTime.now(),
+                                          updatedAt: DateTime.now()));
+                            },
+                            child: const Text("add member"))
+                      ],
                     ),
                   ),
                 ),
-                ElevatedButton(
-                  onPressed: () async {
-                    await ref.read(albumPictureRepository).createAlbumPicture(
-                          albumPicture: AlbumPicture.empty(),
-                          albumId: 'albumPictureTestId',
-                          groupId: "groupIdOf",
-                        );
-                  },
-                  child: Text("album picture test"),
-                ),
-                ElevatedButton(
-                  onPressed: () async {
-                    final pictures = await ref
-                        .read(albumPictureRepository)
-                        .retrieveAlbumPictures(
-                          albumId: 'albumPictureTestId',
-                          groupId: "groupIdOf",
-                        );
-                    print(pictures);
-                  },
-                  child: Text("read"),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+              ),
+            ));
   }
 }
