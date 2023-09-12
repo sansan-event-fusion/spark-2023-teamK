1b108ccf802a47006e39a57e9ec4be881747429e
Ryzhon <pekozono@email.com>
2023-09-12 13:32:02 +0900

    add: delete storage

--- functions/src/modules/deleteGroup.ts
+++ functions/src/modules/deleteGroup.ts
@@ -1,72 +1,93 @@
 import { HttpHandler } from "../types";
-import * as admin from 'firebase-admin';
+import * as admin from "firebase-admin";
+
+import { firestore } from "../lib/firebase";
+const bucket = admin.storage().bucket();
 
 type RequestData = {
-    groupId: string;
+  groupId: string;
 };
 
 type ResponseData = {
-    success: boolean;
-    message?: string;
+  success: boolean;
+  message?: string;
 };
 
-export const deleteSpecificGroup: HttpHandler<RequestData, ResponseData> = async (
-    data,
-    _
-) => {
-    const { groupId } = data;
-    const db = admin.firestore();
+export const deleteSpecificGroup: HttpHandler<
+  RequestData,
+  ResponseData
+> = async (data, _) => {
+  const { groupId } = data;
+  const db = firestore();
 
-    async function deleteDocAndSubCollections(docPath: string) {
-        const docRef = db.doc(docPath);
+  async function deleteDocAndSubCollections(docPath: string) {
+    const docRef = db.doc(docPath);
 
-        const collections = await docRef.listCollections();
-        for (const collection of collections) {
-            const collectionPath = `${docPath}/${collection.id}`;
-            await deleteCollection(collectionPath, 10);
-        }
-
-        await docRef.delete();
+    const collections = await docRef.listCollections();
+    for (const collection of collections) {
+      const collectionPath = `${docPath}/${collection.id}`;
+      await deleteCollection(collectionPath, 10);
     }
 
-    async function deleteCollection(collectionPath: string, batchSize: number) {
-        const collectionRef = db.collection(collectionPath);
-        const query = collectionRef.limit(batchSize);
+    await docRef.delete();
+  }
 
-        const deletedDocs = await deleteQueryBatch(query);
+  async function deleteCollection(collectionPath: string, batchSize: number) {
+    const collectionRef = db.collection(collectionPath);
+    const query = collectionRef.limit(batchSize);
 
-        if (deletedDocs >= batchSize) {
-            // There are more documents to delete
-            return deleteCollection(collectionPath, batchSize);
-        }
+    const deletedDocs = await deleteQueryBatch(query);
 
-        return deletedDocs;
+    if (deletedDocs >= batchSize) {
+      // There are more documents to delete
+      return deleteCollection(collectionPath, batchSize);
     }
 
-    async function deleteQueryBatch(query: FirebaseFirestore.Query) {
-        const snapshot = await query.get();
+    return deletedDocs;
+  }
 
-        const batchSize = snapshot.size;
-        if (batchSize === 0) {
-            return 0;
-        }
+  async function deleteQueryBatch(query: FirebaseFirestore.Query) {
+    const snapshot = await query.get();
 
-        const batch = db.batch();
-        snapshot.docs.forEach((doc) => {
-            batch.delete(doc.ref);
-        });
-
-        await batch.commit();
-
-        return batchSize;
+    const batchSize = snapshot.size;
+    if (batchSize === 0) {
+      return 0;
     }
 
-    try {
-        const groupPath = `groups/${groupId}`;
-        await deleteDocAndSubCollections(groupPath);
+    const batch = db.batch();
+    snapshot.docs.forEach((doc) => {
+      batch.delete(doc.ref);
+    });
 
-        return { success: true };
-    } catch (error) {
-        return { success: false };
+    await batch.commit();
+
+    return batchSize;
+  }
+
+  async function deleteStorageFolder(folderPath: string) {
+    const files = await bucket.getFiles({ prefix: folderPath });
+    for (const file of files[0]) {
+      await file.delete();
     }
+  }
+
+  try {
+    const groupPath = `groups/${groupId}`;
+    await deleteDocAndSubCollections(groupPath);
+
+    const usersRef = db.collection("users");
+    const usersSnapshot = await usersRef.get();
+
+    usersSnapshot.forEach(async (userDoc) => {
+      const userGroupId = userDoc.id;
+      const userGroupPath = `users/${userGroupId}/groups/${groupId}`;
+      await db.doc(userGroupPath).delete();
+    });
+
+    await deleteStorageFolder(`groups/${groupId}`);
+
+    return { success: true };
+  } catch (error) {
+    return { success: false };
+  }
 };
