5af28c6df2bd814eaa92f3b6f3d2ff13b06b181d
Ryzhon <pekozono@email.com>
2023-09-12 11:43:37 +0900

    add: delete user/posts

--- functions/src/modules/deleteUser.ts
+++ functions/src/modules/deleteUser.ts
@@ -1,70 +1,79 @@
 import { HttpHandler } from "../types";
-import * as admin from 'firebase-admin';
+import * as admin from "firebase-admin";
+import { firestore } from "../lib/firebase";
 
 type RequestData = {
-    userId:string,
-    groupId:string
+  userId: string;
+  groupId: string;
 };
 
 type ResponseData = {
-    success: boolean;
-    message?: string;
+  success: boolean;
+  message?: string;
 };
 
 export const deleteUser: HttpHandler<RequestData, ResponseData> = async (
-    _,
-    __
+  _,
+  __
 ) => {
-    const db = admin.firestore();
+  async function deleteCollection(collectionPath: string, batchSize: number) {
+    const collectionRef = firestore.collection(collectionPath);
+    const query = collectionRef.limit(batchSize);
 
-    async function deleteCollection(collectionPath: string, batchSize: number) {
-        const collectionRef = db.collection(collectionPath);
-        const query = collectionRef.limit(batchSize);
+    const deletedDocs = await deleteQueryBatch(query);
 
-        const deletedDocs = await deleteQueryBatch(query);
-
-        if (deletedDocs >= batchSize) {
-            // There are more documents to delete
-            return deleteCollection(collectionPath, batchSize);
-        }
-
-        return deletedDocs;
+    if (deletedDocs >= batchSize) {
+      // There are more documents to delete
+      return deleteCollection(collectionPath, batchSize);
     }
 
-    async function deleteQueryBatch(query: FirebaseFirestore.Query) {
-        const snapshot = await query.get();
+    return deletedDocs;
+  }
 
-        const batchSize = snapshot.size;
-        if (batchSize === 0) {
-            return 0;
-        }
+  async function deleteQueryBatch(query: FirebaseFirestore.Query) {
+    const snapshot = await query.get();
 
-        const batch = db.batch();
-        snapshot.docs.forEach((doc) => {
-            batch.delete(doc.ref);
-        });
-
-        await batch.commit();
-
-        return batchSize;
+    const batchSize = snapshot.size;
+    if (batchSize === 0) {
+      return 0;
     }
 
-    try {
-        const groupsSnapshot = await db.collection('groups').get();
+    const batch = firestore.batch();
+    snapshot.docs.forEach((doc) => {
+      batch.delete(doc.ref);
+    });
 
-        for (const groupDoc of groupsSnapshot.docs) {
-            const groupId = groupDoc.id;
-            const membersSnapshot = await db.collection(`groups/${groupId}/members`).get();
+    await batch.commit();
 
-            for (const memberDoc of membersSnapshot.docs) {
-                const memberId = memberDoc.id;
-                const postsPath = `groups/${groupId}/members/${memberId}/posts`;
-                await deleteCollection(postsPath, 10);
-            }
-        }
+    return batchSize;
+  }
 
-        return { success: true };
-    } catch (error) {
-        return { success: false};
+  try {
+    const groupsSnapshot = await firestore.collection("groups").get();
+
+    for (const groupDoc of groupsSnapshot.docs) {
+      const groupId = groupDoc.id;
+      const membersSnapshot = await firestore
+        .collection(`groups/${groupId}/members`)
+        .get();
+
+      for (const memberDoc of membersSnapshot.docs) {
+        const memberId = memberDoc.id;
+        const postsPath = `groups/${groupId}/members/${memberId}/posts`;
+        await deleteCollection(postsPath, 10);
+      }
     }
+
+    const usersSnapshot = await firestore.collection("users").get();
+
+    for (const userDoc of usersSnapshot.docs) {
+      const userId = userDoc.id;
+      const postsPath = `users/${userId}/posts`;
+      await deleteCollection(postsPath, 10);
+    }
+
+    return { success: true };
+  } catch (error) {
+    return { success: false };
+  }
 };
